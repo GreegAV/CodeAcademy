1480c753aa8d134e9e608c3473d80ce76c38c277
GreegAV <greegav@gmail.com>
2017-07-01 22:05:59 +0300

    Dog updated

--- src/Dog.java
+++ src/Dog.java
@@ -8,7 +8,6 @@ class Dog {
 
     }
 
-
     public void bark() {
 
         System.out.println("Woof!");
@@ -19,12 +18,18 @@ class Dog {
         System.out.println("Your dog ran " + feet + " feet!");
     }
 
+    public int getAge() {
 
+        return age;
+
+    }
 
     public static void main(String[] args) {
         Dog spike = new Dog(5);
         spike.bark();
         spike.run(100);
+        int spikeAge=spike.getAge();
+        System.out.println(spikeAge);
 
     }
 
