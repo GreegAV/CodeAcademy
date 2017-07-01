0ee4669a35c09e855452237b664dfe2d3efc6d05
GreegAV <greegav@gmail.com>
2017-07-01 21:25:19 +0300

    убрал лишние строчки

--- src/Rodentia.java
+++ src/Rodentia.java
@@ -1,13 +1,10 @@
 public class Rodentia {
 
     public static void main(String[] args) {
-
     }
 
     public void order() {
-
         System.out.println("This animal belongs to the Rodentia order.");
-
     }
 
 }
