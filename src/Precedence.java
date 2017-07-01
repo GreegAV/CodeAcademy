f83a79493135193e7198edf8e6106e88b3384f1d
GreegAV <greegav@gmail.com>
2017-07-01 21:22:42 +0300

    Initial commit

---
+++ src/Precedence.java
@@ -0,0 +1,11 @@
+/**
+ * Created by avg-m on 01/07/2017.
+ */
+public class Precedence {
+    public static void main(String[] args) {
+
+        boolean riddle = !( 1 < 8 || (5 > 2 && 3 < 5));
+        System.out.println(riddle);
+
+    }
+}
---
+++ src/Quiz01.java
@@ -0,0 +1,10 @@
+/**
+ * Created by avg-m on 01/07/2017.
+ */
+public class Quiz01 {
+    public static void main(String[] args) {
+        int gemdas = (10 / 5) * 3;
+        System.out.println(5 + 5 != 110 % 100);
+        System.out.println(gemdas);
+    }
+}
